e0e67d9b53f2890994bc87e05a2947bde73c2e59
jarthur <jarthurnmelo1@outlook.com>
2023-10-04 14:56:47 -0300

    New website

---
+++ script.js
@@ -0,0 +1,8 @@
+function darkmode() {
+    let element = document.body;
+    let button = document.getElementById("button")
+    element.classList.toggle("dark-mode");
+    button.classList.toggle("fa-flip-horizontal");
+  }
+
+  alert("Obrigado por entrar no meu site!")
